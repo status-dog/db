64dbe50cab30aaee5cf86cf197f5425a439631e1
Patrick Haun <patrick@bomgar.email>
2022-08-02 16:10:13 +0200

    add user reference

--- db/migrations/20220802132023_create_authenticators.sql
+++ db/migrations/20220802132023_create_authenticators.sql
@@ -1,4 +1,5 @@
 -- migrate:up
+
 create table statusdog.authenticators (
 	id BIGSERIAL not null primary key,
 	---	// SQL: Encode to base64url then store as `TEXT`. Index this column
@@ -8,7 +9,8 @@ create table statusdog.authenticators (
 	---	// SQL: Consider `BIGINT` since some authenticators return atomic timestamps as counters
 	counter bigint not null,
 	---	// SQL: `VARCHAR(255)` and store string array as a CSV string
-	transports text[]
+	transports text[],
+	user_id BIGINT references statusdog.users(id) on delete cascade
 )
 
 
