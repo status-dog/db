d57fa6aeb373326cd028fa42f812ddf488da6951
Patrick Haun <patrick@bomgar.email>
2022-08-09 13:18:44 +0200

    add expiration

--- db/migrations/20220802133239_pending_registrations_table.sql
+++ db/migrations/20220802133239_pending_registrations_table.sql
@@ -1,7 +1,8 @@
 -- migrate:up
 create table statusdog.pending_registrations (
   id BIGINT primary key,
-	email varchar(500) not null,
-	challenge text null
+  email varchar(500) not null,
+  challenge text not null,
+  expires timestamptz not null
 )
 
